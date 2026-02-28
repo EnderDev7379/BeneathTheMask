7aa317bb477674903be4bd65540825c61501914a
OtoDev08 <stefanposao0814@gmail.com>
2026-02-28 14:10:25 +0100

    PLayer script and update

--- player.gd
+++ player.gd
@@ -1,11 +1,20 @@
-extends Sprite2D
-
+extends CharacterBody2D
+var HELTH;
+var V;
+var a;
+var Vmax;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	V = 0;
+	a = 10;
+	Vmax = 100
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	if Input.is_action_pressed("move_up"):
+		if V <= Vmax:
+			V += a*delta
+		position.y -= V;
