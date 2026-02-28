7a52066097c8f123cc5e29b66dcc2d7c2c124990
Gooseman <vukslag@gmail.com>
2026-02-28 17:17:11 +0100

    Add some stuff

--- player.gd
+++ player.gd
@@ -5,6 +5,25 @@ var Vh;
 var a;
 var Vmax;
 
+@onready var textBox = $"../CanvasLayer/ColorRect/Label";
+
+var interaction_stage = 0;
+
+const dialoges: Array[Array] = [
+	[
+"Zdravo spasioče,  hvala ti što možeš da nam pomogneš.",
+"Zdravo, oćeš na kafu.",
+"Šta si došao kod nas? Zar ne znaš da dolazi propast u ovo selo, odlazi odavde!!!"
+	]
+];
+
+const descriptions: Array[String] = [
+"Ovaj čovek je seljak, radi ceo dan u njivi ima ženu i dve mačke. 
+Resurs koji donosi su žitarice.",
+"Ovaj čovek je voćar, on gaji razno voće i ima sina Janka.
+Resurs koji donosi su voće i Janko."
+];
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Vv = 0;
@@ -13,14 +32,15 @@ func _ready() -> void:
 	Vmax = 20;
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("move_up"):
+		$ShapeCast2D.rotation_degrees = 180;
 		if Vv <= Vmax:
 			Vv -= a*delta
 		
 	elif Input.is_action_pressed("move_down"):
+		$ShapeCast2D.rotation_degrees = 0;
 		if Vv <= Vmax:
 			Vv += a*delta
 		
@@ -30,10 +50,12 @@ func _process(delta):
 		else:
 			Vv = 0;
 	if Input.is_action_pressed("move_left"):
+		$ShapeCast2D.rotation_degrees = 90;
 		if Vh <= Vmax:
 			Vh -= a*delta
 		
 	elif Input.is_action_pressed("move_right"):
+		$ShapeCast2D.rotation_degrees = 270;
 		if Vh <= Vmax:
 			Vh += a*delta
 		
@@ -43,3 +65,18 @@ func _process(delta):
 		else:
 			Vh = 0;
 	move_and_collide(Vector2(Vh, Vv));
+	
+	if Input.is_action_just_pressed("Interaction"):
+		if (get_tree().paused):
+			interaction_stage += 1
+		var raycast: ShapeCast2D = $ShapeCast2D;
+		if raycast.is_colliding():
+			var collider: Node = raycast.get_collider(0);
+			if collider.get_parent() == $"../Npcs":
+				$"../CanvasLayer/ColorRect".visible = true;
+				if (interaction_stage == 0):
+					textBox.text = 
+				elif (interaction_stage == 1):
+					textBox.text = descriptions[collider.get_meta("id")];
+				get_tree().paused = true;
+			
