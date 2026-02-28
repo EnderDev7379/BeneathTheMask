9c2a4c5fbdad99f1ec9e3a62c1dddc50ac024fa3
OtoDev08 <stefanposao0814@gmail.com>
2026-02-28 14:14:56 +0100

    Player script update

--- player.gd
+++ player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 var HELTH;
-var V;
+var Vv;
+var Vh;
 var a;
 var Vmax;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	V = 0;
+	Vv = 0;
+	Vh = 0;
 	a = 10;
 	Vmax = 100
 	pass # Replace with function body.
@@ -15,6 +17,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("move_up"):
-		if V <= Vmax:
-			V += a*delta
-		position.y -= V;
+		if Vv <= Vmax:
+			Vv += a*delta
+		position.y -= Vv;
+	if Input.is_action_pressed("move_down"):
+		if Vv <= Vmax:
+			Vv += a*delta
+		position.y += Vv;
+	if Input.is_action_pressed("move_left"):
+		if Vh <= Vmax:
+			Vh += a*delta
+		position.x -= Vh;
+	if Input.is_action_pressed("move_right"):
+		if Vh <= Vmax:
+			Vh += a*delta
+		position.x += Vh;
