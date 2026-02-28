b271ff796ee97ad7ca618cd4a59e00e59ab2ee20
Gooseman <vukslag@gmail.com>
2026-02-28 19:51:57 +0100

    LOTS OF STUFF

--- interaction.gd
+++ interaction.gd
@@ -26,8 +26,95 @@ const descriptions: Array[Array] = [
 	[
 "Ovaj čovek je seljak, radi ceo dan u njivi ima ženu i dve mačke. 
 Resurs koji donosi su žitarice.",
+"Ovaj čovek je žena od seljaka, oni zajedno žive sa dve mačke. Ona takođe zna da kuva.
+Resurs koji donosi je spremanje hrane.",
+"Ovaj čovek je stara baba koja ima farmu kokoški.
+Resurs koji donosi su kokoške i njihova jaja.",
+"Ovaj čovek je star, on je bolestan, ali ima bunar vode.
+Resurs koji donosi je voda.",
+"Ovaj čovek je mlad i ujak mu je Tesla.
+Resurs koji donosi je struja.",
+"Ovaj čovek je riba. Treba mu voda.",
+"Ovaj čovek je ribar, on lovi ribu.
+Resurs koji donosi je riba i morski plodovi.",
 "Ovaj čovek je voćar, on gaji razno voće i ima sina Janka.
-Resurs koji donosi su voće i Janko."
+Resurs koji donosi su voće i Janko.",
+"Ja sam sin Janko, moji ljubimci su ptice koje lete kroz očev voćnjak.",
+"Ovaj čovek je mesar u selu, hrani okolne mačke lutalice.
+Resurs koji donosi je meso.",
+"Ovaj čovek ima problem jer mu lisica stalno ubija živinu.
+Resurs koji donosi je preživela živina.",
+"Ovaj čovek ima tiho jezero iza kuće. Kaže da voda pamti sve.",
+"Ovaj mladi mehaničar ume da pokrene i pokvareni agregat.
+Resurs koji donosi je mogućnost popravljanja pokvarenog.",
+"Ovaj čovek je besan pekar jer ljudi ne vole njegovu pekaru.
+Resurs koji donosi su peciva iz njegove pekare.",
+"Ovaj čovek je žena koja zna da napravi kombinaciju trava, koje kad se zapale opuštaju ljude.
+Resurs koji donosi su trave za opuštanje.",
+"Ovaj čovek je policajac i on zna ko su dileri u selu.
+Resurs koji donosi je red i mir.",
+"Ovaj čovek je sveštenik. Očistiće ti dušu od greha sa molitvom.
+Resurs koji donosi je vera u Boga i sveće.",
+"Ovaj čovek je detektiv koji sumnja na šumara.
+Resurs bolji mozak nego ti.",
+"Ovaj čovek je trgovac koji prodaje slatkiše.
+Resurs koji donosi su slatkiši.",
+"Ovaj čovek krade od kleptomanijaka i daje siromašnima.",
+"Ovaj čovek nema smisla. On ne zna ništa. Možete da ne izaberite njega.",
+	],
+	[
+"Ovaj čovek je žena od seljaka, oni žive zajedno sa dva psa. Ona takođe zna da kuva.
+Resurs koji donosi je spremanje hrane.",
+
+"Ovaj čovek je stari mlekar sa desert krava.
+Resurs koji donosi su paradajz i krastavac.",
+
+"Ovaj čovek je kanibal, ali on je doktor.
+Resurs koji donosi je da te i leči i jede.",
+
+"Ovaj čovek je star, on nike verovao u fiziku i testirao zakon gravitacije i kao posledica nastradala je od jednog seljaka krava.
+Resurs koji donosi je ljubav prema fizici.",
+
+"Ja sam ćerka Janko, mnogo volim svog psa.",
+
+"Ovaj čovek je dobio besnilo od lisice.",
+
+"Ovaj čovek neće da plati pekaru jer je hleb bajat.",
+
+"Ovaj čovek je policajac koji pomaže čoveku sa travama da ih dovede do svojih kupaca.
+Resurs koji donosi su red i mir, kontakt od gospodina sa travama.",
+
+"Ovaj čovek je gospodin sa travama koji tačno zna koja ti vrsta trave treba. Njegove trave leče mozak.
+Resurs koji donosi su perece.",
+
+"Ovaj čovek je šumar u šumi u kojoj nestaju ljudi, on pokušava da ih nađe.",
+
+"Ovaj čovek je pokeraš koji svoje pare daje u dobrotvorne svrhe.
+Resurs koji donosi su pare.",
+
+"Ovaj čovek je banker u selu, ali ne zna gde je banka.
+Resurs koji donosi je demencija.",
+
+"Ovaj čovek je gospodin čekanja, verovatno beskoristan.",
+
+"Ovaj čovek kaže da trgovac prodaje otrovane slatkiše.",
+
+"Ovaj čovek je kleptomanijak.
+Resurs koji donosi su ukradene životinje. ",
+
+"Ovaj čovek sveštenik koji crta interesantne krogove.
+Resurs koji donosi je sveta piće.",
+
+"Ovaj čovek je sedeo sa detektivom u policijskoj stanici.",
+
+"Ovaj čovek je profesor hemije, njegovo ime je Wolter White.",
+
+"Ovaj čovek će ti seen-ovati poruke.",
+
+"Ovaj čovekne voli gospodina Ribu.",
+
+"Ovaj čovek se preselio u selo jer je ubio čoveka.
+Resurs koji donosi je mrtvo telo."
 	]
 ];
 
---
+++ npcs.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+@onready var npcs: Node2D = $"."
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	for npc in npcs.get_children():
+		if (npc.get_meta("type") == -1):
+			npc.set_meta("type", randi_range(0, 1));
+		if (npc.gett_meta("id") == -1):
+			npc.set_meta("id", randi_range(0, 21) if npc.get_meta("type") == 1 else randi_range(0, 20))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
